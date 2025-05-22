df4a91d5483b9f10f6b58e4d98889976ae2e6ded
mdnur3330 <hmnuralomseddeki@gmail.com>
2025-05-22 14:47:28 +0600

    adding Top Rating Best Selling section

--- src/Pages/TopRatingBestSelling.jsx
+++ src/Pages/TopRatingBestSelling.jsx
@@ -1,120 +1,225 @@
-import React from 'react';
-import item from "../assets/item-instagram-2.jpg"
-import item2 from "../assets/item-instagram-3.jpg"
-import item3 from "../assets/item-instagram-4.jpg"
+import React from "react";
+import item from "../assets/item-instagram-2.jpg";
+import item2 from "../assets/item-instagram-3.jpg";
+import item3 from "../assets/item-instagram-4.jpg";
+import product1 from "../assets/product-item-1.jpg";
+import product8 from "../assets/product-item-17.jpg";
+import product2 from "../assets/product-item-2.jpg";
+import product3 from "../assets/product-item-20.jpg";
+import product4 from "../assets/product-item-21.jpg";
+import product5 from "../assets/product-item-4.jpg";
+import product6 from "../assets/product-item-6.jpg";
+import product7 from "../assets/product-item-7.jpg";
+
 const TopRatingBestSelling = () => {
-    return (
+  return (
+    // <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    //   <div className="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
+    //     <h2 className="max-w-lg mb-5 font-sans text-3xl font-medium tracking-tight sm:text-4xl sm:leading-none md:mb-6 group">
+    //       <span className="inline-block mb-1 sm:mb-4">
+    //         Top Rating
+    //         <br className="hidden md:block" />
+    //        Best Selling
+    //       </span>
+    //       <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
+    //     </h2>
+    //     <p className="lg:text-sm lg:max-w-md">
+    //       "Class is dead", says Foucault; however, according to Reicher, it is
+    //       not so much class that is dead, but rather the absurdity, and some
+    //       would say the paradigm, of class. However, the subject is interpolated
+    //       into a postpatriarchialist dialectic theory that includes
+    //       consciousness as a totality.
+    //     </p>
+    //   </div>
+    // <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    //   <div className="grid gap-10 lg:grid-cols-2">
+    //     <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+    //       <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+    //         <svg className="w-7 h-7" viewBox="0 0 24 24">
+    //           <polyline
+    //             fill="none"
+    //             stroke="currentColor"
+    //             strokeWidth="2"
+    //             strokeLinecap="round"
+    //             strokeMiterlimit="10"
+    //             points=" 8,5 8,1 16,1 16,5"
+    //             strokeLinejoin="round"
+    //           />
+    //           <polyline
+    //             fill="none"
+    //             stroke="currentColor"
+    //             strokeWidth="2"
+    //             strokeLinecap="round"
+    //             strokeMiterlimit="10"
+    //             points="9,15 1,15 1,5 23,5 23,15 15,15"
+    //             strokeLinejoin="round"
+    //           />
+    //           <polyline
+    //             fill="none"
+    //             stroke="currentColor"
+    //             strokeWidth="2"
+    //             strokeLinecap="round"
+    //             strokeMiterlimit="10"
+    //             points="22,18 22,23 2,23 2,18"
+    //             strokeLinejoin="round"
+    //           />
+    //           <rect
+    //             x="9"
+    //             y="13"
+    //             fill="none"
+    //             stroke="currentColor"
+    //             strokeWidth="2"
+    //             strokeLinecap="round"
+    //             strokeMiterlimit="10"
+    //             width="6"
+    //             height="4"
+    //             strokeLinejoin="round"
+    //           />
+    //         </svg>
+    //       </div>
+    //       <div className="max-w-xl mb-6">
+    //         <h2 className="max-w-lg mb-6 font-sans text-3xl font-medium tracking-tight
+    //         sm:text-4xl sm:leading-none">
+    //          Beautiful plant
+
+    //           <br className="hidden md:block" />
+    //           stands that encourage you{' '}
+    //           <span className="inline-block text-deep-purple-accent-400">
+    //             to get creative.
+    //           </span>
+    //         </h2>
+    //         <p className="text-base md:text-lg">
+
+    //         </p>
+    //       </div>
+    //     </div>
+    //     <div className="flex items-center justify-center -mx-4 lg:pl-8">
+    //       <div className="flex flex-col items-end px-3">
+    //         <img
+    //           className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+    //           src={item}
+    //           alt=""
+    //         />
+    //         <img
+    //           className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+    //           src={item2}
+    //           alt=""
+    //         />
+    //       </div>
+    //       <div className="px-3">
+    //         <img
+    //           className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+    //           src={item3}
+    //           alt=""
+    //         />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    // </div>
 
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
-        <h2 className="max-w-lg mb-5 font-sans text-3xl font-medium tracking-tight sm:text-4xl sm:leading-none md:mb-6 group">
-          <span className="inline-block mb-1 sm:mb-4">
-            Top Rating
-            <br className="hidden md:block" />
-           Best Selling
-          </span>
-          <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
-        </h2>
-        <p className="lg:text-sm lg:max-w-md">
-          "Class is dead", says Foucault; however, according to Reicher, it is
-          not so much class that is dead, but rather the absurdity, and some
-          would say the paradigm, of class. However, the subject is interpolated
-          into a postpatriarchialist dialectic theory that includes
-          consciousness as a totality.
-        </p>
-      </div>
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="grid gap-10 lg:grid-cols-2">
-        <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
-          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
-            <svg className="w-7 h-7" viewBox="0 0 24 24">
-              <polyline
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                points=" 8,5 8,1 16,1 16,5"
-                strokeLinejoin="round"
-              />
-              <polyline
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                points="9,15 1,15 1,5 23,5 23,15 15,15"
-                strokeLinejoin="round"
-              />
-              <polyline
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                points="22,18 22,23 2,23 2,18"
-                strokeLinejoin="round"
-              />
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="1d4040f3-9f3e-4ac7-b117-7d4009658ced"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
               <rect
-                x="9"
-                y="13"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                width="6"
-                height="4"
-                strokeLinejoin="round"
+                fill="url(#1d4040f3-9f3e-4ac7-b117-7d4009658ced)"
+                width="52"
+                height="24"
               />
             </svg>
-          </div>
-          <div className="max-w-xl mb-6">
-            <h2 className="max-w-lg mb-6 font-sans text-3xl font-medium tracking-tight 
-            sm:text-4xl sm:leading-none">
-             Beautiful plant
+            <span className="relative">Top Rating</span>
+          </span>{" "}
+          Best Selling
+        </h2>
+        <p className="text-base text-gray-700 md:text-lg">
+          Show Off Your Green Style with Beautiful Plant Stands Elevate your
+          indoor garden with stylish plant stands that do more than just hold
+          your plants — they inspire creativity. Whether you prefer a minimalist
+          look, a boho vibe, or a modern aesthetic, our beautifully crafted
+          plant stands let you mix, match, and design your own green corners
+          just the way you like. Add height, dimension, and personality to any
+          space — and turn your plants into art.
+        </p>
+      </div>
 
-
-              <br className="hidden md:block" />
-              stands that encourage you{' '}
-              <span className="inline-block text-deep-purple-accent-400">
-                to get creative.
-              </span>
-            </h2>
-            <p className="text-base md:text-lg">
-           Show Off Your Green Style with Beautiful Plant Stands
-Elevate your indoor garden with stylish plant stands that do more than just hold your plants — they inspire creativity. Whether you prefer a minimalist look, a boho vibe, or a modern aesthetic, our beautifully crafted plant stands let you mix, match, and design your own green corners just the way you like. Add height, dimension, and personality to any space — and turn your plants into art.
-            </p>
-          </div>
+      <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product1}
+            alt="Person"
+          />
         </div>
-        <div className="flex items-center justify-center -mx-4 lg:pl-8">
-          <div className="flex flex-col items-end px-3">
-            <img
-              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-              src={item}
-              alt=""
-            />
-            <img
-              className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-              src={item2}
-              alt=""
-            />
-          </div>
-          <div className="px-3">
-            <img
-              className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-              src={item3}
-              alt=""
-            />
-          </div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product2}
+            alt="Person"
+          />
+        </div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product3}
+            alt="Person"
+          />
+        </div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product4}
+            alt="Person"
+          />
+        </div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product5}
+            alt="Person"
+          />
+        </div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product6}
+            alt="Person"
+          />
+        </div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product7}
+            alt="Person"
+          />
+        </div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={product8}
+            alt="Person"
+          />
         </div>
       </div>
-    </div>
- 
     </div>
   );
 };
-
-
 
 export default TopRatingBestSelling;
